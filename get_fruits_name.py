2c937dd6b74a04b07f201bf7149f651737c45256
Davron17 <dorazaliyev75@gmail.com>
2023-10-24 23:41:33 +0500

    fruits_name

--- get_fruits_name.py
+++ get_fruits_name.py
@@ -7,6 +7,15 @@ def get_frutis_name(data:str)->list:
     returns:
         list: list of fruits names
     """
-    return
+    from csv import reader 
+    f=reader(data)
+    c=0
+    for i in f:
+        if c==0:
+            c+=1
+            continue
+        print(i[0])
+f=open("fruits.csv")
+get_frutis_name(f)
 
     
